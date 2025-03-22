037dc20b9a398165f9537d1cf3c087392df5e0dd
Parry-Zhuo <165851545+Parry-Zhuo@users.noreply.github.com>
2025-03-22 14:35:09 -0700

    Update lcdDisplay.sv
    first revision of lcdDisplay written. 
    
    Timing may be a bit overkill between cycles
    Have not yet tested or compiled this module
    
    Pseudocode has been implemented

--- lcdDisplay.sv
+++ lcdDisplay.sv
@@ -1,3 +1,8 @@
+// ELEX 7660
+// File: lcdDisplay.sv
+// Description: provides LCD display
+// Author: Parry Zhuo Date: 2025-03-22
+
 
 module lcd_controller (
     input logic CLOCK_50,// 50 MHz clock// System clock
@@ -7,13 +12,10 @@ module lcd_controller (
     output logic        E,         //The LCD reads data only on the falling edge of E (from HIGH → LOW). For bits DB7-0
     output logic [7:0]  data,       // Data bus
     output logic [15:3] GPIO_0 //this should be in the top module.
-	 
 );
     logic [4:0] count, next_count;
     logic [15:0] clk_div_count; // count used to divide clock
     logic clk ; 
-
-
 
     // State machine states
     typedef enum logic [2:0] {
@@ -27,101 +29,104 @@ module lcd_controller (
     } state_t;
 
     state_t state, next_state;
-
-    // Initialization sequence
-    always_ff @(posedge clk or posedge rst) begin
+    /*Loading data block
+    UPON a rising edge of E, data and states are transitioned to next data/state
+    changed to provide stable signal when E becomes a falling edge.
+    */
+    always_ff @(posedge E or rst) begin
         if (rst) begin
             state <= INIT;
+            next_state <= INIT;
+            count <= 0;
+            next_count <= 0;
             RS    <= 0;
             RW    <= 0;
             E     <= 0;
             data  <= 8'b0;
         end else begin
-            state <= next_state;
             case (state)
-                INIT: begin
-                    // Wait for power stabilization
-                    // Transition to FUNCTION_SET after delay
+                state <= next_state;
+                count <= next_count;
+                INIT: begin// Wait for power stabilization
                     //Wait 15ms - if VDD>4.5V
-                end FUNCTION_SET: begin
+                    E    <= 1;//my hope with this is that it triggers the timer
+                end FUNCTION_SET: begin// set to 8 bit length
                     RS   <= 0;
                     RW   <= 0;
-                    data <= 8'b0011_1000; // 8-bit, 2-line, 5x8 dots <-- may
-                    E    <= 1;// <- not exactly sure what this does
-
-                    // Transition to DISPLAY_CONTROL wait 4.1ms
+                    data <= 8'b0011_0000;
+                    E    <= 1;
                 end DISPLAY_CONTROL: begin
                     RS   <= 0;
                     RW   <= 0;
                     data <= 8'b0000_1100; // Display ON, Cursor OFF, Blink OFF
                     E    <= 1;
-                    // Transition to ENTRY_MODE
-                    //wait 100us
                 end ENTRY_MODE: begin
                     RS   <= 0;
                     RW   <= 0;
                     data <= 8'b0000_0110; // Increment mode
                     E    <= 1;
-                    // Transition to CLEAR_DISPLAY
-
-                    //maybe delay 1.6ms
                 end CLEAR_DISPLAY: begin
                     RS   <= 0;
                     RW   <= 0;
                     data <= 8'b0000_0001; // Clear display
                     E    <= 1;
-                    // Transition to READY
-
-                    //maybe delay 40us. 
                 end READY: begin
                     // Ready for data input load data in
                     RS   <= 1;// ensure we are in write mode always
                     data <= /* data to write */;
                     EN <= 0 
-                    //wait maybe 1ms or something
                 end WRITE_DATA: begin
                     RS   <= 1;// ensure we are in write mode always
                     RW   <= 0;
                     E    <= 1; //WRITE DATA TRIGGERS UPON FALLING EDGE from E
-
                     // Transition back to READY
                 end
             endcase
         end
     end
-   // use count to divide clock and generate a 2 bit digit counter to determine which digit to display
-    always_ff @(posedge CLOCK_50) 
-        clk_div_count <= clk_div_count + 1'b1 ;
-
-    assign clk = clk_div_count[6];//2^6 = 1024
+   /*
+   Counter block, triggers e 
+    if e==next_count/2 then
+        e = 0// creates falling edge to trigger reading data on the LCD chip
+    else if e == 0 then
+        e = 1// creates rising edge to trigger next sequence of data and counters
+   */  
+    always_ff(@posedge clk) begin
+        if(count < next_count/2 && count > 0) begin
+            e = 0; // creates falling edge for inserting data on steadyWave data waveforms
+        end else begin
+            e = 1;// Otherwise tie e to 1 just incase of unexpected error
+        end
+        count = count - 1'b1;
+    end
 
     // Next state logic
     always_comb begin
         if(count > 0) begin
             next_count = count - 1'b1;
-            next_state = state; 
+            next_state = state;     
         end else begin
             if(state == INIT) begin
                 next_state = FUNCTION_SET;
                 next_count = 150-1;
             end else if (state == FUNCTION_SET) begin
                 next_state = DISPLAY_CONTROL;
-                next_count = 0;
+                next_count = 2;
             end else if (state == DISPLAY_CONTROL) begin
                 next_state = ENTRY_MODE;
-                next_count = 0;
+                next_count = 2;
             end else if (state == ENTRY_MODE) begin
                 next_state = CLEAR_DISPLAY;
-                next_count = 0;
+                next_count = 2;
             end else if (state == CLEAR_DISPLAY) begin
                 next_state = READY;
-                next_count = 0;
+                next_count = 2;
             end else if (state == READY) begin
                 next_state = WRITE_DATA;
-                next_count = 0;
+                next_count = 2;
             end else if (state == WRITE_DATA) begin
                 next_state = READY;
-                next_count = 0;
+                next_count = 2;
             end else begin
                 next_state = INIT;
                 next_count = 150-1;
@@ -136,5 +141,9 @@ module lcd_controller (
 		GPIO[15:7] =data[7:0];       // Data bus
 	end 
 
+    always_ff @(posedge clock) 
+        clk_div_count <= clk_div_count + 1'b1 ;
+
+    assign clk = clk_div_count[6];//2^6 = 1024
 
 endmodule
